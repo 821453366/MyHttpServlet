37cb1614dc16b9ee51c42a4286d29de232a7fcb5
821453366 <821453366@qq.com>
2017-03-05 17:53:01 +0800

    修改添加AServlet文件

---
+++ src/MyServlet/AServlet.java
@@ -0,0 +1,22 @@
+package MyServlet;
+
+import java.io.IOException;
+
+/**
+ * Created by pc on 17-3-5.
+ */
+@javax.servlet.annotation.WebServlet(name = "Servlet",urlPatterns = "/AServlet")
+public class AServlet extends javax.servlet.http.HttpServlet {
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        response.getWriter().println("<h1>账号："+username+"</h1> <br> <h1>密码："+password+"</h1>");
+    }
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+       doPost(request,response);
+    }
+
+
+}
